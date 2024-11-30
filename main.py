4472a4dc8d818368266336301edb6b14d8722958
Ivlion <gleb.ivlev66@gmail.com>
2024-11-30 10:36:51 +0300

    Git и случайные окружности

--- main.py
+++ main.py
@@ -1,18 +1,53 @@
 import sys
-from PyQt6 import QtWidgets
 from PyQt6.QtCore import QPoint
 from PyQt6.QtGui import QColor, QPainter, QPixmap
 from random import randint
 from PyQt6 import uic
 from PyQt6.QtWidgets import QMainWindow
+from PyQt6 import QtCore, QtGui, QtWidgets
 
 
-class MainWindow(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(334, 302)
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(110, 230, 91, 31))
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.label = QtWidgets.QLabel(parent=self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, 10, 311, 211))
+        self.label.setText("")
+        self.label.setObjectName("label")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 334, 18))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Кнопка"))
+        self.pushButton.setText(_translate("MainWindow", "НАЖМИ"))
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.flag = True
         self.pushButton.released.connect(self.run)
+        self.window().setFixedSize(self.window().size())
 
     def run(self):
         self.flag = True
@@ -26,7 +61,7 @@ class MainWindow(QMainWindow):
             radius = randint(10, 50)
             center_x = self.label.width() // 2 + randint(-radius, radius)
             center_y = self.label.height() // 2 + randint(-radius, radius)
-            paint.setBrush(QColor("yellow"))
+            paint.setBrush(QColor(randint(1, 255), randint(1, 255), randint(1, 255), randint(1, 255)))
             paint.drawEllipse(QPoint(center_x, center_y), radius, radius)
             self.label.setPixmap(pixmap)
             paint.end()
